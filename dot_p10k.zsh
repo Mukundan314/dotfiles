a63e6215138bea3ae2633e6f269148c19e21ee02
Mukundan314 <mukundan314@gmail.com>
2020-12-28 11:55:10 +0530

    Show os identifier on the right

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -99,6 +99,7 @@
     # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
+    os_icon                 # os identifier
   )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
