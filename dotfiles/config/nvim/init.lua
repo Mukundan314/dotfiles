e31ae8a0e8936df0ea3a9b712ea9bd6d2d8bb53d
Mukundan314 <30190448+Mukundan314@users.noreply.github.com>
2022-07-22 14:21:02 +0530

    Setup catppuccin theme for neovim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,1 +1,15 @@
 vim.o.clipboard = "unnamedplus"
+vim.o.colorcolumn = "80,120"
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap("n", "<Leader><Space>", ":nohlsearch<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end })
+
+vim.cmd [[packadd! catppuccin]]
+
+vim.cmd [[colorscheme catppuccin]]
