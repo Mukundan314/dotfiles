ec8d866295cd505b068b1e47dd826e39d8f2456c
Mukundan314 <30190448+Mukundan314@users.noreply.github.com>
2023-01-09 12:19:50 +0530

    Configure nvim-cmp to use vim-vsnip as snippet engine

--- dotfiles/config/nvim/lua/configure.lua
+++ dotfiles/config/nvim/lua/configure.lua
@@ -59,9 +59,12 @@ end
 
 function configure.nvim_cmp()
   vim.cmd("packadd! nvim-cmp")
+  vim.cmd("packadd! vim-vsnip")
+
   local cmp = require("cmp")
 
   cmp.setup({
+    snippet = { expand = function(args) vim.fn["vsnip#anonymous"](args.body) end },
     mapping = cmp.mapping.preset.insert({
       ["<C-p>"] = cmp.mapping.select_prev_item(),
       ["<C-n>"] = cmp.mapping.select_next_item(),
